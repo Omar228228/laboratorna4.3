8f3e9140e8470fcdee231f508fc6bd91936336f2
Omar228228 <goblock542@gmail.com>
2021-10-06 10:49:02 +0300

    first commit

---
+++ Laboratorna4.3.cpp
@@ -0,0 +1,48 @@
+﻿// Lab_04_3.cpp
+// Виконав студент Ткачишин Юрій
+// Лабораторна робота № 4.3
+// Табуляція функції, заданої формулою: функція з параметрами.
+// Варіант 10.
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, a, b, c, F;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "c = "; cin >> c;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		if (x < 3.0 && b != 0) //Умова 1
+		{
+			F = ((a * (x * x)) + ((b * b) * x));
+		}
+		else
+			if (x > 3.0 && b == 0)//Умова 2
+			{
+				F = ((x - a) / (x - c));
+			}
+			else
+			{
+				F = (x / c);
+			}
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << F
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
